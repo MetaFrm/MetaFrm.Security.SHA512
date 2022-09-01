823daddda9c8161c6200cf5bf7844de604887d0c
DSUN <dsun@hanmail.net>
2022-09-01 14:57:46 +0900

    초기 커밋.

---
+++ README.md
@@ -0,0 +1,3 @@
+[![.NET](https://github.com/dsuny/MetaFrm.Security.SHA512/actions/workflows/dotnet.yml/badge.svg)](https://github.com/dsuny/MetaFrm.Security.SHA512/actions/workflows/dotnet.yml)
+
+# MetaFrm.Security.SHA512
---
+++ src/SHA512.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MetaFrm.Security
+{
+    /// <summary>
+    /// SHA512 해시 알고리즘
+    /// </summary>
+    public class SHA512 : IHashAlgorithm
+    {
+        string IHashAlgorithm.ComputeHash(string value)
+        {
+            return Convert.ToBase64String(((IHashAlgorithm)this).ComputeHash(Encoding.UTF8.GetBytes(value)));
+        }
+
+        string IHashAlgorithm.ComputeHash(string value, Encoding? encoding)
+        {
+            if (encoding == null)
+                encoding = Encoding.UTF8;
+
+            return Convert.ToBase64String(((IHashAlgorithm)this).ComputeHash(encoding.GetBytes(value)));
+        }
+
+        byte[] IHashAlgorithm.ComputeHash(byte[] value)
+        {
+            byte[] bytes;
+
+            using HashAlgorithm sHAManaged = System.Security.Cryptography.SHA512.Create();
+            bytes = sHAManaged.ComputeHash(value);
+            sHAManaged.Clear();
+
+            return bytes;
+        }
+    }
+}
