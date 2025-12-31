d704aa5eb975b4d63c472fab2487a5a9ef59af43
권대선 <dsun@hanmail.net>
2025-12-31 14:19:59 +0900

    2025-12-31_01

--- src/SHA512.cs
+++ src/SHA512.cs
@@ -1,5 +1,4 @@
-﻿using System.Security.Cryptography;
-using System.Text;
+﻿using System.Text;
 
 namespace MetaFrm.Security
 {
@@ -10,26 +9,19 @@ namespace MetaFrm.Security
     {
         string IHashAlgorithm.ComputeHash(string value)
         {
-            return Convert.ToBase64String(((IHashAlgorithm)this).ComputeHash(Encoding.UTF8.GetBytes(value)));
+            return ((IHashAlgorithm)this).ComputeHash(value);
         }
 
         string IHashAlgorithm.ComputeHash(string value, Encoding? encoding)
         {
-            if (encoding == null)
-                encoding = Encoding.UTF8;
+            encoding ??= Encoding.UTF8;
 
             return Convert.ToBase64String(((IHashAlgorithm)this).ComputeHash(encoding.GetBytes(value)));
         }
 
         byte[] IHashAlgorithm.ComputeHash(byte[] value)
         {
-            byte[] bytes;
-
-            using HashAlgorithm sHAManaged = System.Security.Cryptography.SHA512.Create();
-            bytes = sHAManaged.ComputeHash(value);
-            sHAManaged.Clear();
-
-            return bytes;
+            return System.Security.Cryptography.SHA512.HashData(value);
         }
     }
 }
